ae8148b6c538400cf79b4fe1d120c1981ed9a4bb
zhanhaoruan <zhanhaor@uci.edu>
2024-11-21 15:06:56 -0800

    Sort the final index by token freqeuency for easier query later

--- src/merger.py
+++ src/merger.py
@@ -32,9 +32,9 @@ def merge_partial_indexes():
                 print(f"Error processing {p_file}: {e}")
 
         print("\nSorting final index entries...")
-        # Sort postings for each token by Document ID
+        # Sort postings for each token by Frequency
         for token in final_index:
-            final_index[token].sort(key=lambda x: x[0])
+            final_index[token].sort(key=lambda x: x[1])
 
         # Save final inverted index
         final_index_path = os.path.join(FINAL_INDEX_DIR, 'final_inverted_index.json')
