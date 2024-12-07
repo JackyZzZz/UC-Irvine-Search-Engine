cad624624cd180a1718fcab338e434a06a0c0723
zhanhaoruan <zhanhaor@uci.edu>
2024-12-07 15:45:33 -0800

    implement tf-df scoring

--- src/search.py
+++ src/search.py
@@ -9,70 +9,39 @@ def search_with_query(limit, *args):
     stemer = PorterStemmer()
     results = []
 
-    # Load the inverted_index and doc mapping
-    # with open(f"{FINAL_INDEX_DIR}/final_inverted_index.json") as file:
-    #     data = json.load(file)
     with open(DOC_MAPPING_FILE) as file:
         map = json.load(file)
-
+    
     total_num_docs = len(map)
-    term_document_num_map = {}
+
+
 
     # Stem the query terms
-    stemmed_terms = [stemer.stem(term) for term in args]
+    stemmed_terms = sorted([stemer.stem(term) for term in args])
 
-    docs_term_frequency_map = {}
+    docs_scores_map = {}
     
-    # Get document sets for each term
-    term_docs = []
+    previous_letter = None
+
     for term in stemmed_terms:
         starting_letter = term.lower()[0]
-        with open(f"{FINAL_INDEX_DIR}/{starting_letter}_tokens.json") as file:
-            data = json.load(file)
+        if starting_letter != previous_letter:
+            with open(f"{FINAL_INDEX_DIR}/{starting_letter}_tokens.json") as file:
+                data = json.load(file)
+            previous_letter = starting_letter
         if term in data:
+            document_frequency = len(data[term])
             for posting in data[term]:
-                if posting[0] in docs_term_frequency_map:
-                    docs_term_frequency_map[posting[0]].append((term, posting[1]))
+                if posting[0] in docs_scores_map:
+                    docs_scores_map[posting[0]] += (1 + log(posting[1])) * (1 + log(total_num_docs/document_frequency))
                 else:
-                    docs_term_frequency_map[posting[0]] = [(term, posting[1])]
-            docs = set(posting[0] for posting in data[term])
-            term_document_num_map[term] = len(docs)
-            term_docs.append(docs)
-        else:
-            return []
+                    docs_scores_map[posting[0]] = (1 + log(posting[1])) * (1 + log(total_num_docs/document_frequency))
+            else:
+                continue
 
-    # Intersect all document sets
-    if term_docs:
-        result_set = term_docs[0]
-        for docs in term_docs[1:]:
-            result_set = result_set.intersection(docs)
-
-        # Calculate tf-df scores
-        doc_scores = {}
-        for doc_id in result_set:
-            score = 0
-            for token, frequency in docs_term_frequency_map[doc_id]:
-                score += (1 + log(frequency)) * (1 + log(total_num_docs/term_document_num_map[token]))
-            doc_scores[doc_id] = score
-
-        # Calculate scores and deduplicate by base URL
-        # url_scores = {}  # Dict to store highest score for each base URL
-        # for doc_id in result_set:
-        #     url = map[str(doc_id)]
-        #     # Remove fragment from URL (everything after #)
-        #     base_url = url.split('#')[0]
-            
-        #     # Calculate total frequency for this document
-        #     total_freq = sum(posting[1] for term in stemmed_terms 
-        #                    for posting in data[term] 
-        #                    if posting[0] == doc_id)
-            
-        #     # Keep only highest scoring version of each URL
-        #     if base_url not in url_scores or total_freq > url_scores[base_url][1]:
-        #         url_scores[base_url] = (doc_id, total_freq)
-
-        # Sort by frequency and output top k unique results
-        sorted_results = sorted(doc_scores.items(), key=lambda x: x[1], reverse=True)
+    # Output results
+    if docs_scores_map:
+        sorted_results = sorted(docs_scores_map.items(), key=lambda x: x[1], reverse=True)
         for doc_id, _ in sorted_results:
             print(map[str(doc_id)])
             url = map[str(doc_id)]
